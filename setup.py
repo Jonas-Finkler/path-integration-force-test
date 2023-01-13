560e33269380d8601c6d70679bfd7a3a732a494a
moritzgubler <moritz.gubler@gmail.com>
2023-01-13 08:06:48 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pathIntegrationForceTest',
+    version='1.0.0',    
+    description='An example Python package',
+    packages=['pathIntegrationForceTest'],
+    install_requires=['numpy',
+                      'ase',         
+                      ],
+)
